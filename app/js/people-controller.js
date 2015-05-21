1a9be71d9297d84e506b80ef4a08fe92b51cc980
Anne Kelly <annekellyc@gmail.com>
2015-05-21 14:02:18 -0300

    Anne - a simple example using angular and tests =).

--- app/js/people-controller.js
+++ app/js/people-controller.js
@@ -1,3 +1,21 @@
-angular.module('angular-dojo').controller('PeopleController', ['$scope', function ($scope) {
+angular.module('angular-dojo')
+.controller('PeopleController', ['$scope', function ($scope) {
 	
+	$scope.people = [];
+	$scope.person = "";
+	$scope.add = function (name) {
+		$scope.people.push({ name: name, selected: false });
+	};
+	
+	$scope.clear = function () {
+		$scope.people = [];
+		$scope.person = "";
+	};
+	
+	$scope.peopleSelected = [];
+	$scope.$watch('people|filter:{selected:true}', function (peopleFiltered) {
+    $scope.peopleSelected = peopleFiltered.map(function (person) {
+      return person.name;
+    });
+  }, true);
 }]); 
--- test/js/people-controller-spec.js
+++ test/js/people-controller-spec.js
@@ -6,14 +6,31 @@ describe("People Controller", function() {
     $rootScope = $injector.get('$rootScope');
 		$scope = $rootScope.$new();
 		$controller = $injector.get('$controller');
-  }));
-	
-	it("insert person name", function() {
+		
 		$controller('PeopleController', {
 			'$scope': $scope
 		});
+  }));
+	
+	it("add a person", function() {
+		$scope.add('Anne');
+		$scope.add('Marcelo');
+		$scope.people.should.be.deep.equal([{ name: "Anne", selected: false }, { name: 'Marcelo', selected: false }]);
+	});
+	
+	it("clear person item", function () {
+		$scope.add('Anne');
+		$scope.clear();
+		$scope.people.should.be.deep.equal([]);
+	});
+	
+	it("selected people", function () {
+		$scope.add("Anne");
+		$scope.add("Marcelo");
 		
-		$scope.insertPerson('Anne');
-		$scope.people.should.be.deep.equal(['Anne']);
+		$scope.people[0].selected = true;
+		$scope.$apply();
+		
+		$scope.peopleSelected.should.be.deep.equal(["Anne"]);
 	});
 });
